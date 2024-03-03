b7e46c26a07f7ed00bf88baa0580edbaf5ee962e
Kennidy Stillwell <you@example.com>
2024-03-02 22:48:05 -0500

    update signup

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -15,7 +15,7 @@ const SignUpForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('/signup', {
+      const response = await fetch('http://localhost:5000/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
